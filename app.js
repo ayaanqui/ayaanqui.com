7dc5cc0514441ce7b854fb435eac8739ae0f9754
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-23 22:45:21 -0500

    Added 404 page

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(home);
 app.use(projects);
 app.use(about);
+// 404 error page
+app.use((req, res, next) => {
+  res.render('404', {
+    pageInfo: {
+      title: '404 page not found'
+    }
+  });
+});
 
 app.listen(3000);
 console.log("Listening to port 3000: http://localhost:3000/");
