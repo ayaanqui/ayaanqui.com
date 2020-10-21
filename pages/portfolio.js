f08185c14ffe7816a9077b1da2ad4f1c70e13c6c
ayaanqui <moahammedayaan.dev@gmail.com>
2020-10-21 18:02:08 -0500

    Added keys to group-list-item

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -29,7 +29,12 @@ class Portfolio extends React.Component {
         <div className="list-group">
           {
             this.state.repos.map(repo => (
-              <a href={repo.html_url} target="_blank" className="list-group-item list-group-item-action">
+              <a
+                href={repo.html_url}
+                target="_blank"
+                className="list-group-item list-group-item-action"
+                key={repo.id}
+              >
                 {repo.name}
               </a>
             ))
