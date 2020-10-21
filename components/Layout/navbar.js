aa59ed1bd48ac4f0aed76af484e1693bd2f5c0c3
ayaanqui <moahammedayaan.dev@gmail.com>
2020-10-21 18:03:30 -0500

    Implemented NavLink

--- components/Layout/navbar.js
+++ components/Layout/navbar.js
@@ -1,35 +1,23 @@
 import Link from "next/link";
+import NavLink from "./navLink";
 
 const Navbar = props => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
-      <a className="navbar-brand" href="/">Ayaan Siddiqui</a>
+      <Link href="/"><a className="navbar-brand">Ayaan Siddiqui</a></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link href='/'>
-              <a className="nav-link">Home <span className="sr-only">(current)</span></a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="portfolio">
-              <a className="nav-link">Portfolio</a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="resume">
-              <a className="nav-link">Resume</a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="about">
-              <a className="nav-link">About</a>
-            </Link>
-          </li>
+          <NavLink href="/"><a className="nav-link">Home</a></NavLink>
+
+          <NavLink href="/portfolio"><a className="nav-link">Portfolio</a></NavLink>
+
+          <NavLink href="/resume"><a className="nav-link">Resume</a></NavLink>
+
+          <NavLink href="/about"><a className="nav-link">About</a></NavLink>
         </ul>
       </div>
     </nav>
