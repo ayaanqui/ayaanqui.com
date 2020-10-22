c120cb6cdeb5c902995421b461d3a2a8ddf63978
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-10-22 00:51:43 -0500

    Navbar sm

--- components/Layout/navbar.js
+++ components/Layout/navbar.js
@@ -3,7 +3,7 @@ import NavLink from "./navLink";
 
 const Navbar = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-transparent">
       <Link href="/"><a className="navbar-brand">Ayaan Siddiqui</a></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
