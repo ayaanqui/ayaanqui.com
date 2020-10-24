77948f91aee59b389f14f3ce8649992c491e1d11
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-10-24 13:08:40 -0500

    Updated home page style and content

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,43 @@
 import Header from '../components/header';
 import Link from 'next/link';
 import Content from '../components/Layout/content';
+import styles from './index.module.css';
 
 export default function Home() {
   return (
-    <Content>
+    <Content navInvisible={true}>
       <Header />
+      
+      <div className={styles.homeContent}>
+        <div className={styles.title}>
+          <h1>Ayaan Siddiqui</h1>
+        </div>
 
-      <div className="container mt-5 mb-5">
-        <h1>Welcome!</h1>
+        <div className={styles.homeNav}>
+          <ul className={styles.homeNavList}>
+            <li className={styles.homeNavListItem}>
+              <Link href="/portfolio">
+                <a className="nav-link">Portfolio</a>
+              </Link>
+            </li>
+            <li className={styles.homeNavListItem}>
+              <Link href="/resume">
+                <a className="nav-link">Resume</a>
+              </Link>
+            </li>
+            <li className={styles.homeNavListItem}>
+              <Link href="/about">
+                <a className="nav-link">About</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-        <br />
-
-        <p>
-          This webstite is still under works, with only the portfolio page working. In the near future, I hope to display my resume, projects, interests, and contact information on here.
-        </p>
+        <div className={styles.summary}>
+          <p>
+            This webstite is still under works, with only the portfolio page working. In the near future, I hope to display my resume, projects, interests, and contact information on here.
+          </p>
+        </div>
       </div>
     </Content>
   )
