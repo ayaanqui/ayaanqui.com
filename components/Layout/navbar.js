8c9519c3a6fe6666604b3b15ec6e127a49c99e8a
ayaanqui <moahammedayaan.dev@gmail.com>
2020-11-02 00:44:52 -0600

    Updated homepage

--- components/Layout/navbar.js
+++ components/Layout/navbar.js
@@ -4,21 +4,23 @@ import NavLink from "./navLink";
 const Navbar = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-transparent">
-      <Link href="/"><a className="navbar-brand">Ayaan Siddiqui</a></Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <div className="container">
+        <Link href="/"><a className="navbar-brand">Ayaan Siddiqui</a></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <NavLink href="/"><a className="nav-link">Home</a></NavLink>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <NavLink href="/"><a className="nav-link">Home</a></NavLink>
 
-          <NavLink href="/portfolio"><a className="nav-link">Portfolio</a></NavLink>
+            <NavLink href="/portfolio"><a className="nav-link">Portfolio</a></NavLink>
 
-          <NavLink href="/resume"><a className="nav-link">Resume</a></NavLink>
+            <NavLink href="/resume"><a className="nav-link">Resume</a></NavLink>
 
-          <NavLink href="/about"><a className="nav-link">About</a></NavLink>
-        </ul>
+            <NavLink href="/about"><a className="nav-link">About</a></NavLink>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,40 +4,34 @@ import Content from '../components/Layout/content';
 import styles from './index.module.css';
 
 export default function Home() {
+  const skills = [
+    'Java', 'Node.js', 'C/C++', 'Python', 'React', 'Express.js', 'Next.js', 'MySQL', 'PostgreSQL'
+  ];
+
   return (
-    <Content navInvisible={true}>
+    <Content>
       <Header />
       
-      <div className={styles.homeContent}>
+      <div className={`${styles.homeContent} container`}>
         <div className={styles.title}>
-          <h1>Ayaan Siddiqui</h1>
+          <h1>Hi there!</h1>
         </div>
-
-        <div className={styles.homeNav}>
-          <ul className={styles.homeNavList}>
-            <li className={styles.homeNavListItem}>
-              <Link href="/portfolio">
-                <a className="nav-link">Portfolio</a>
-              </Link>
-            </li>
-            <li className={styles.homeNavListItem}>
-              <Link href="/resume">
-                <a className="nav-link">Resume</a>
-              </Link>
-            </li>
-            <li className={styles.homeNavListItem}>
-              <Link href="/about">
-                <a className="nav-link">About</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-
+        
         <div className={styles.summary}>
           <p>
-            Currently a student at the University of Illinois at Chicago studying computer science. Fluent in Java, JavaScript (Node.js), and C++. When I'm not coding or doing school related work, I enjoy spending time learning martial arts such as BJJ, Muay Thai, and Wrestling.
+            I'm a student at University of Illinois at Chicago studying computer science. When I'm not coding or doing school-related work, I enjoy learning martial arts such as Jiu Jitsu, Kickboxing, and Wrestling.
           </p>
         </div>
+      </div>
+
+      <div className="container mb-5">
+        <h4>Programming skills</h4>
+
+        <br />
+
+        {skills.map(skill => (
+          <button type="button" className={`btn btn-dark ${styles.styleBtn}`} key={skill.toLowerCase()}>{skill}</button>
+        ))}
       </div>
     </Content>
   )
