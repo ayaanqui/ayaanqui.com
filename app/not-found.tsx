471cf7535faa0610505e9a4dfbda300e8a6b1cad
ayaanqui <ayaanqui.com@gmail.com>
2023-09-10 18:54:49 -0500

    fix: 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,19 @@
-import Content from "../components/layout/Content";
+"use client";
 
-const Error404Page = () => {
+import Content from "./components/layout/Content";
+
+export default function Error404Page() {
   return (
     <Content>
-      <div className="container mt-5 mb-5">
+      <div className="container mt-10 mb-5">
         <div className="text-center mb-3">
-          <h1>404, Page not found</h1>
+          <h1>Page not found</h1>
         </div>
-        <p>
+        <p className="text-center">
           The page that you are trying to access does not exist. Try using the
           links in the navbar to navigate through the different pages.
         </p>
       </div>
     </Content>
   );
-};
-
-export default Error404Page;
+}
